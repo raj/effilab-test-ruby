025e6f8976518ac66d29ef096b47a1519a5ace26
raj <raj.deenoo@gmail.com>
2018-08-18 21:42:25 +0200

    parallel request for Adwords CampaignService

--- lib/ad_reporter/providers/adwords.rb
+++ lib/ad_reporter/providers/adwords.rb
@@ -12,6 +12,7 @@ module AdReporter
         super(provided_config)
         @api_version = @config[:api_version].nil? ? DEFAULT_API_VERSION : @config[:api_version].to_sym
         @client = AdwordsApi::Api.new(@config[:filename])
+        @campaigns = []
       end
 
       def authorize
@@ -20,11 +21,11 @@ module AdReporter
 
       def process
         stats = {nb_ad_groups: 0, nb_keywords: 0, nb_campaigns: 0}
-        campaigns = get_campaigns
+        get_campaigns
         all_campaigns = []
 
-        workers = campaigns.length < 10 ? 1 : 10
-        Parallel.each(lambda { campaigns.pop || Parallel::Stop }, :in_threads => workers) { |data|
+        workers = @campaigns.length < 10 ? 1 : 10
+        Parallel.each(lambda { @campaigns.pop || Parallel::Stop }, :in_threads => workers) { |data|
           data[:nb_ad_groups] = get_number_of_ad_groups data[:id]
           all_campaigns << data
         }
@@ -45,10 +46,10 @@ module AdReporter
         File.join(ENV["HOME"], AdwordsApi::ApiConfig.default_config_filename)
       end
 
-      def get_campaigns
-        campaigns = []
+      def get_campaigns_for_page(offset = 0, number_per_page = 3)
         campaign_srv = @client.service(:CampaignService, @api_version)
-
+        # Set initial values.
+        page = {}
         # Get all the campaigns for this account.
         selector = {
           :fields => ["Id", "Name", "Status"],
@@ -57,26 +58,30 @@ module AdReporter
           ],
           :paging => {
             :start_index => 0,
-            :number_results => 500,
+            :number_results => number_per_page,
           },
         }
-
-        # Set initial values.
-        offset, page = 0, {}
-
         begin
           page = campaign_srv.get(selector)
           if page[:entries]
             page[:entries].each do |campaign|
-              campaigns << {id: campaign[:id], name: campaign[:name], status: campaign[:status]}
+              @campaigns << {id: campaign[:id], name: campaign[:name], status: campaign[:status]}
             end
-            # Increment values to request the next page.
-            offset += 500
-            selector[:paging][:start_index] = offset
           end
-        end while page[:total_num_entries] > offset
+        end
+        page[:total_num_entries]
+      end
 
-        campaigns
+      def get_campaigns
+        number_per_page = 500
+        total_num_entries = get_campaigns_for_page(0, number_per_page)
+        number_of_page = total_num_entries / number_per_page
+        return if number_of_page == 0
+
+        workers = total_num_entries < number_per_page * 2 ? 1 : 10
+        Parallel.each(lambda { (1...number_of_page + 1).to_a.pop || Parallel::Stop }, :in_threads => workers) { |index|
+          get_campaigns_for_page(index * number_per_page, number_per_page)
+        }
       end
 
       def get_number_of_ad_groups(campaign_id)
