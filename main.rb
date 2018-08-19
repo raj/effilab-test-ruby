4d5900cd73b641b28e5fbe4d3c35b7439af90c0c
raj <raj.deenoo@gmail.com>
2018-08-19 16:13:34 +0200

    clean main.rb & ad_reporter script

--- main.rb
+++ main.rb
@@ -2,12 +2,6 @@ $LOAD_PATH << File.dirname(__FILE__)
 $LOAD_PATH << File.join(File.dirname(__FILE__), "lib")
 require "lib/ad_reporter"
 
-DEFAULT_OUTPUT = AdReporter::Outputs::PrettyStdout.new
-
-adwords_provider = AdReporter::Providers::Adwords.new
-output_manager = AdReporter::OutputManager.new
-output_manager.add DEFAULT_OUTPUT
-
 reporter = AdReporter::Reporter.new
 reporter.authorize
 reporter.run
